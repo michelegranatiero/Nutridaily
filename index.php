463a682f35a04a8fa004f0ceb291833d611042f7
Michele Granatiero <mikigranatiero@hotmail.com>
2022-05-18 19:54:49 +0200

    ok

--- index.php
+++ index.php
@@ -108,7 +108,9 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
+            <input type="button" class="form-control" value="" id="searchedItem">
             <input type="text" class="form-control" placeholder="Inserisci un alimento" id="searchText" spellcheck="false">
+            
             <!-- live search -->
             <div id="display" ></div>
 
@@ -137,8 +139,11 @@
     function vistaPasto() {
       //reset search on close
       if (!($(tbody).is($(event.target).closest("div")))){
-        $('#searchText').val(null);
+        $('#searchText').val("");
         $("#display").html("");
+        $('#searchedItem').hide();
+        $('#searchedItem').val("");
+        $('#searchText').show();
       }
 
       tbody = $(event.target).closest("div");
@@ -153,14 +158,31 @@
     /* live search.......... */
     function fill(value) {
       //Assigning value to "search" div in "search.php" file.
-      $('#searchText').val(value);
-        //Hiding "display" div in "search.php" file.
+      $('#searchedItem').val(value);
+      //Hiding "display" div in "search.php" file.
       $('#display').hide();
+      $("#searchText").hide();
+      $('#searchedItem').show();
+      $('#searchText').val(null);
+
     }
 
     $(document).ready(function () {
 
+      $('#accordion .panel').on("click", function() {
+        $(this).siblings().find(".panel-heading").removeClass("panel-heading-active");
+        $(this).find(".panel-heading").toggleClass("panel-heading-active");
+      });
+
       //live search-------------------------------------------------------------------
+      $('#searchedItem').hide();
+
+      $('#searchedItem').click(function(){
+        $('#searchedItem').hide();
+        $('#searchText').show();
+        $('#searchedItem').val("");
+      });
+
       //On pressing a key on "Search box" in "search.php" file. This function will be called.
       $("#searchText").keyup(function() {
           //Assigning search box value to javascript variable named as "txt".
@@ -186,6 +208,7 @@
                   //If result found, this funtion will be called.
                   success: function(html) {
                       //Assigning result to "display" div in "search.php" file.
+                      $('#display').show();
                       $("#display").html(html).show;
                   }
               });
@@ -195,8 +218,8 @@
 
       //button aggiungi alimento
       $("#add-button").click(function () {
-        var alimento = $('#searchText').val();
-        if (!($(alimento).is(':empty'))) {
+        if ($('#searchedItem').val()!="") {
+          var alimento = $('#searchedItem').val();
           count += 1;
           let template1 = `
             <!-- header tabella -->
@@ -219,13 +242,16 @@
               </div>
             </div>
           `;
+          console.log($('#searchedItem').val());
+          $('#searchedItem').val("");
+          console.log($('#searchedItem').val());
           var expand = tbody.next(); //da espandere dopo
           tbody = tbody.next().find(".accordion-body");
           $(tbody).append(template1);
           if (!$(expand).hasClass("show")){
             $(expand).collapse('toggle'); //espansione al click
           }
-          $('#searchText').val(null); //azzera search
+          ; //azzera search
         }
         else {
           alert('Inserisci un alimento');
